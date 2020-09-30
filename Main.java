cdb6f3f6985b0b9dc26e2404a4886d84392231da
edcaamal <edcaamal@uacam.mx>
2020-09-30 03:37:09 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+
+  static void imprimirMensaje(String mensaje){
+    System.out.println(mensaje);
+  }
+
+  public static void main(String[] args) {
+    imprimirMensaje("========================");
+  }
+}
